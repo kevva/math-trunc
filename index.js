d0c79d61bd495eee1ada6c233a78d2a31e6a9bf3
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-21 13:27:54 +0200

    Tweaks

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 module.exports = function (val) {
 	return val < 0 ? Math.ceil(val) : Math.floor(val);
 };
--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 'use strict';
-
 var test = require('ava');
 var mathTrunc = require('./');
 
 test(function (t) {
-	t.assert(mathTrunc(13.37) === 13)
-	t.assert(mathTrunc(42.84) === 42)
-	t.assert(mathTrunc(0.123) === 0)
-	t.assert(mathTrunc(-0.123) === -0)
-	t.assert(mathTrunc('-1.123') === -1)
+	t.assert(mathTrunc(13.37) === 13);
+	t.assert(mathTrunc(42.84) === 42);
+	t.assert(mathTrunc(0.123) === 0);
+	t.assert(mathTrunc(-0.123) === -0);
+	t.assert(mathTrunc('-1.123') === -1);
 	t.assert(isNaN(mathTrunc(NaN)));
 	t.assert(isNaN(mathTrunc('foo')));
-	t.assert(isNaN(mathTrunc()))
+	t.assert(isNaN(mathTrunc()));
 	t.end();
 });
